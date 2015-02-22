8e56044cbb73b23cfd80857d630a09bca7959729
Jimmy Byrd <jimmybyrd@gmail.com>
2015-02-21 19:04:46 -0500

    Nowin server running

---
+++ src/Server/Program.fs
@@ -0,0 +1,22 @@
+﻿open System
+open Microsoft.Owin.Hosting
+open Owin
+open System.Threading.Tasks
+
+
+
+[<EntryPoint>]
+let main argv = 
+
+    let options = StartOptions()
+    options.ServerFactory <- "Nowin"
+    options.Port <- Nullable(8080)
+    let startup (app: IAppBuilder) =
+        
+        ()
+    use server = WebApp.Start(options, startup)
+
+    Console.ReadLine() |> ignore
+
+    printfn "%A" argv
+    0 // return an integer exit code
