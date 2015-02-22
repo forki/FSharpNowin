cde6a52658889a4ab79e59252497c659e27e1820
Jimmy Byrd <jimmybyrd@gmail.com>
2015-02-21 19:50:07 -0500

    Added nancy

--- src/Server/Program.fs
+++ src/Server/Program.fs
@@ -2,21 +2,34 @@
 open Microsoft.Owin.Hosting
 open Owin
 open System.Threading.Tasks
+open Nancy
 
-
+let processor ctx c = None |> async.Return
+    
+type HomeModule() as this =
+    inherit NancyModule()
+    do this.Before.AddItemToEndOfPipeline(
+                fun ctx c -> Async.StartAsTask(async {
+                    let! res = processor ctx c
+                    return match res with 
+                                | Some x -> x
+                                | None -> null
+                }))
+    do this.Get.["/"] <- (fun param -> 
+                            "Hello world" :> obj)
 
 [<EntryPoint>]
 let main argv = 
-
     let options = StartOptions()
     options.ServerFactory <- "Nowin"
-    options.Port <- Nullable(8080)
+    let port = 8080
+    options.Port <- Nullable(port)
     let startup (app: IAppBuilder) =
-        
+        app.UseNancy() |> ignore
         ()
     use server = WebApp.Start(options, startup)
-
+    
+    printfn "Listening on port %d" port
     Console.ReadLine() |> ignore
+    0
 
-    printfn "%A" argv
-    0 // return an integer exit code
